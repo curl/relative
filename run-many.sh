6020a20a072e0fde219b60f2466a08c945122563
Daniel Stenberg <daniel@haxx.se>
2021-08-25 10:47:47 +0200

    run-many: run sprinter using all installed libcurl versions

---
+++ run-many.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# use installed versions
+
+pwd=`pwd`
+
+cd build
+
+ALL=`ls`;
+
+cd $pwd
+for ver in $ALL; do
+    minor=`echo $ver | cut -d. -f2`;
+    SPRINTER=./sprinter
+    if test "$minor" -lt 66; then
+        SPRINTER=./sprinter-old
+    fi
+    if ! test -x $SPRINTER; then
+        echo "$SPRINTER is missing, forget to build?"
+        exit
+    fi
+    LD_LIBRARY_PATH=$pwd/build/$ver/lib $SPRINTER localhost/4KB 100000 10 2>/dev/null
+done
